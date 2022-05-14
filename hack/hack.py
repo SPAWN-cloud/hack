0b4cff052573c6d52d1e46ed209818421ca40230
spawnn <80365034+SPAWN-cloud@users.noreply.github.com>
2022-05-14 14:35:08 -0300

    Add files via upload

---
+++ hack/hack.py
@@ -0,0 +1,43 @@
+#-- coding: UTF-8 --
+import os, time, json, requests, socket, pty
+from sys import exit
+reset = '\033[0;0m'
+destq = '\033[;1m'
+yellow = '\033[1;33m'
+green = '\033[1;32m'
+red = '\033[1;31m'
+blue = '\033[1;34m'
+wh = '\033[1;97m'
+
+print(f'''
+          .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+                               coded by xSp4wn_
+
+                !!a tool pode demorar ate 15 mins para carregar toda!! ''')
+
+
+print(f''' {red}
+[1] Hackear por IP
+[2] Hackear por Reverse Shell
+[3] Criar Backdoor 
+''')
